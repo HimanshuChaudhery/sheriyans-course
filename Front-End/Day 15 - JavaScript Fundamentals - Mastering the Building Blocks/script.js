dbf95ab101d3dded940b597e4e66417eb29e192f
Himanshu <himanshuchaudhery962@gmail.com>
2025-09-13 19:55:55 +0530

    JavaScript Fundamentals still remaining

---
+++ Front-End/Day 15 - JavaScript Fundamentals - Mastering the Building Blocks/script.js
@@ -0,0 +1,44 @@
+// console.log("Hello");
+// let a = 10;
+// console.log(a);
+
+/* Datatypes in JS
+    1. Primitive
+        - Numbers (10, 9, 9.1, 8.12) range:: 2^53-1
+        - String ('a','Hello', "CARS")
+        - Boolean (true and false)
+        - undefined : variable is declared but not initialized
+        - null
+        - Symbol: it is used in advance concept when creating unique values
+        - BigInt: for very big integer numbers
+
+    2. Reference or Derived
+        - Array
+        - Object
+        - Function
+*/
+
+// var b = 20;
+// var c = "Hello";
+// var d = true;
+// console.log(a, b, c, d)
+
+
+// // Taking input from user
+// prompt("Enter you age :"); // It has issue
+// // Issue with prompt is that whatever value we take from the user. It always stores in string which can create problem. When we want to take integer value.
+
+// //Example Below:-
+// var num1 = prompt("Enter your 1st number: ");
+// var num2 = prompt("Enter your 2nd number: ");
+// // console.log(num1+num2); // Here addition is not happening instead it's actually Concatination
+// // console.log(c+d); // Example of concatination
+
+// // To solve the issue with prompt we would simply you type conversion
+// var num1Real = Number(num1);
+// var num2Real = Number(num2);
+// console.log(num1Real+num2Real);
+
+
+// HOISTING: 
+Continue from 36:19
