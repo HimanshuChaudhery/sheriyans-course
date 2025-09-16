9016b23a953c173cccb0eecb06aa92e719308241
Himanshu <himanshuchaudhery962@gmail.com>
2025-09-16 15:21:29 +0530

    Not Done YET!

---
+++ Front-End/Day 17 - Deep Dive into JavaScript Functions/script.js
@@ -0,0 +1,19 @@
+// FUNCTIONS IN JAVASCRIPT
+/*
+    Function is a block of code that can be reuse and runs only executes whenever it is called.
+*/
+
+// Why we need function?
+/*
+    When we want to implement a feature that needs to be run at a particular time and also needs to be used in many scenerios then instead of writing the same code multiple time we will just use the function.
+*/
+
+function greet() // creating a function
+{
+    console.log("Hello World!");
+}
+
+greet(); // function call
+
+
+// How to pass a parameter to a function
