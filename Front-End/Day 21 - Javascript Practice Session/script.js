258da2bf103d17788f8658f2ffab958eaa129e71
Himanshu <himanshuchaudhery962@gmail.com>
2026-07-15 00:36:49 +0530

    Not Completed

---
+++ Front-End/Day 21 - Javascript Practice Session/script.js
@@ -0,0 +1,43 @@
+// Ques 1
+console.log("Hello, Javascript!");
+console.warn("Hello, World!");
+console.error("Hello, Sun");
+console.table({name: "Himanshu", age: 26});
+
+// Ques 2
+var n1 = 35;
+var n2 = 10;
+var n3 = 7;
+
+var ans = n1*2-(n2/2)+n3;
+console.log(ans);
+
+// Ques 3
+let a1 = "123";
+let a2 = 123;
+let a3 = true;
+let a4 = null;
+console.log(typeof a1);
+console.log(typeof a2);
+console.log(typeof a3);
+console.log(typeof a4);
+
+// typeof works on the concept of tags.
+// In javascript every single thing has a data tag.
+// Now what happened they accidentally give Object a datatag 000 and they give the same tag to null.
+// So, whenever someone goes for typeof on null it will give datatype as object.
+
+// Ques 4
+var b1 = 10;
+var b2 = 12;
+var temp = 0;
+
+temp = b1;
+b1 = b2;
+b2 = temp;
+
+console.log(b1 +" "+ b2);
+
+// Ques 5
+
+22:77
